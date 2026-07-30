49e8b01aa5086550edd6e8f6a7e2db323199aed0
hfireboy113-lgtm <h.fireboy113@gmail.com>
2026-07-30 23:26:28 +0330

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-sys.stdout.reconfigure(encoding='utf-8')
+sys.stdout.reconfigure(encoding="utf-8")
 
 from core.scanner import Scanner
 from messenger import send_message
@@ -10,6 +10,7 @@ from messenger import send_message
 def main():
 
     chat_id = os.getenv("CHAT_ID")
+    channel_chat_id = os.getenv("CHANNEL_CHAT_ID")
 
     if not chat_id:
         print("CHAT_ID not found")
@@ -28,12 +29,20 @@ def main():
             result
         )
 
-        print("Message sent to Bale")
+        print("Message sent to Bale chat")
+
+        if channel_chat_id:
+
+            send_message(
+                channel_chat_id,
+                result
+            )
+
+            print("Message sent to Bale channel")
 
     finally:
 
         scanner.close()
-
 
 
 if __name__ == "__main__":
